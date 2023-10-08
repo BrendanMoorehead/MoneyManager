ad620301ddcfa513b961264cb36b5b2cae827400
Brendan Moorehead <bmoorehe@uoguelph.ca>
2023-10-07 20:14:06 -0400

    Import database configuration

--- money-manager/src/index.js
+++ money-manager/src/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Firebase imports
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getAnalytics } from "firebase/analytics";
+
+// Database configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAH818IlVPg9z6hQWprrPoquK67tVBW4iQ",
+  authDomain: "money-manager-f05ea.firebaseapp.com",
+  projectId: "money-manager-f05ea",
+  storageBucket: "money-manager-f05ea.appspot.com",
+  messagingSenderId: "876736763104",
+  appId: "1:876736763104:web:0501154b27bc6686c16c85",
+  measurementId: "G-PRTJ7MLSPM"
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +26,11 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+//TODO: Learn about analytics values
+const analytics = getAnalytics(app);
+
+export { db, auth, analytics };
